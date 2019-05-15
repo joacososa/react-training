094f5f7fe0270f9fc3c9db7ba83a62160c03fc2c
joacososa <joaquinsosa20@hotmail.com>
2019-05-15 20:04:34 -0300

    add via json

--- src/components/productList/ProductList.jsx
+++ src/components/productList/ProductList.jsx
@@ -1,27 +1,12 @@
 import React from 'react';
 import Product from './Product/Product';
+import data from './products.json';
 
 class ProductList extends React.Component {
   constructor(){
       super();
       this.state = {
-          products: [
-            {
-                id: 1,
-                name: 'Vino',
-                description: 'Alamos Malbec'
-            },
-            {
-                id: 2,
-                name: 'Cerveza',
-                description: 'Ipa Atomica'
-            },
-            {
-                id:3,
-                name: 'Fernet',
-                description: 'Branca Clasico'
-             }
-         ]
+          products: data.products
       }
   }
     
