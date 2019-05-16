c373790096419939316d7ee593544348426a4e35
joacososa <joaquinsosa20@hotmail.com>
2019-05-15 22:02:02 -0300

    validate form

--- README.md
+++ README.md
@@ -1,11 +1,67 @@
-## Ejercicio 2
+### Ejercicio 3
 
-## Agregar Producto
-    - Crear Modal con react-bootstrap
-    - Crear boton que muestre el modal
-    - Agregar al modal, formulario para rellenar name, description e imageUrl
-    - Al guardar actualizar lista de productos con el nuevo producto agregado
+## Agregar Validacion al formulario utilizando Formik con Yup
+    Proceso de instalacion de formik y yup
+    #`npm install formik yup --save`
 
-![ejercicio-cards](https://github.com/joacososa/react-training/blob/2-agregar-productos/images-tutorial/2-add-product.PNG)
+    En el Componente del formulario 
+    #`import { Formik } from  'formik';`
+    #`import * as yup from 'yup';` 
+
+## Background:
+
+   - Fromik es una libreria react que permite validar y manipular formularios de forma muy eficiente
+   - Esta se combina de forma excelente con yup libreria que permite crear schemas
+
+## Pasos
+    Definir schema por sobre el form
+    ```        
+        const schema = yup.object({
+            name: yup.string().required(),
+            });
+    ```    
+
+    
+    Agregar componente formik como padre del form
+    ```        
+                <Formik
+                    validationSchema={schema}
+                    onSubmit={this.onSubmit}
+                    >
+                    {({
+                        handleSubmit,
+                        handleChange,
+                        handleBlur,
+                        values,
+                        touched,
+                        isValid,
+                        errors,
+                    }) => (
+                    <Form noValidate onSubmit={handleSubmit}>
+                    .....
+                    .....
+    ```    
+
+    Realizar validacion sobre el From.Control 
+    ```        
+    <Form.Group controlId="formBasicName">
+        <Form.Label>Nombre</Form.Label>
+        <Form.Control
+            name="name"
+            onChange={handleChange}
+            type="text"
+            value={values.name}
+            placeholder="Agregar nombre."
+            onBlur={handleBlur}
+            isInvalid={!!errors.name}
+        />
+        <Form.Control.Feedback type="invalid">
+            {errors.name}
+        </Form.Control.Feedback>
+    </Form.Group>
+    ```  
+
+
+![ejercicio-cards](https://github.com/joacososa/react-training/blob/2-agregar-productos/images-tutorial/3-validate-form.PNG)
 
 Lograr montar la estructura de la imagen.
--- src/components/productList/ProductForm/ProductForm.jsx
+++ src/components/productList/ProductForm/ProductForm.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import { Modal, Button, Form } from 'react-bootstrap';
+import { Modal, Button, Form, InputGroup } from 'react-bootstrap';
 import PropTypes from 'prop-types';
+import { Formik } from  'formik';
+import * as yup from 'yup'; 
+
+const schema = yup.object({
+  name: yup.string().required(),
+  description: yup.string().required(),
+  imageUrl: yup.string().required(),
+});
 
 class ProductForm extends React.Component {
   constructor(){
       super();
       this.state = {
         show: false,
-        product : {
-            name: '',
-            description: '',
-            imageUrl: '',
-        }
       }
   }
 
@@ -23,24 +26,11 @@ class ProductForm extends React.Component {
 
   handleClose = () => {
     this.setState({
-        show: false,
-        product : {
-            name: '',
-            description: '',
-            imageUrl: '',
-    }});
+        show: false});
   }
 
-  onTextElementChanged = (event) => {
-      this.setState({product: {
-        ...this.state.product,  
-        [event.target.name] : event.target.value,
-      }
-    })
-  }
-
-  onSubmit = () => {
-      this.props.onProductAdded(this.state.product);
+  onSubmit = (product) => {
+      this.props.onProductAdded(product);
       this.setState({show: false});
   }
 
@@ -55,29 +45,79 @@ class ProductForm extends React.Component {
                     <Modal.Title>Agregar Producto</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                <Form>
-                    <Form.Group controlId="formBasicName">
-                        <Form.Label>Nombre</Form.Label>
-                        <Form.Control name="name" onChange={this.onTextElementChanged} type="text" placeholder="Agregar nombre." />
-                    </Form.Group>
-                    <Form.Group controlId="formBasicDesc">
-                        <Form.Label name="description">Descripcion</Form.Label>
-                        <Form.Control onChange={this.onTextElementChanged} type="text" placeholder="Agregar descripcion." />
-                    </Form.Group>
-                    <Form.Group controlId="formBasicImageUrl">
-                        <Form.Label>Url de Imagen</Form.Label>
-                        <Form.Control name="imageUrl" onChange={this.onTextElementChanged} type="text" placeholder="Agregar url de imagen del producto" />
-                    </Form.Group>
+                    <Formik
+                    validationSchema={schema}
+                    onSubmit={this.onSubmit}
+                    >
+                    {({
+                        handleSubmit,
+                        handleChange,
+                        handleBlur,
+                        values,
+                        touched,
+                        isValid,
+                        errors,
+                    }) => (
+                    <Form noValidate onSubmit={handleSubmit}>
+                        <Form.Group controlId="formBasicName">
+                            <Form.Label>Nombre</Form.Label>
+                            <Form.Control
+                                name="name"
+                                onChange={handleChange}
+                                type="text"
+                                value={values.name}
+                                placeholder="Agregar nombre."
+                                onBlur={handleBlur}
+                                isValid={touched.name && !errors.name}
+                                isInvalid={!!errors.name}
+                            />
+                            <Form.Control.Feedback type="invalid">
+                                {errors.name}
+                            </Form.Control.Feedback>
+                        </Form.Group>
+                        <Form.Group controlId="formBasicDesc">
+                            <Form.Label name="description">Descripcion</Form.Label>
+                            <Form.Control 
+                                onChange={handleChange} 
+                                value={values.description}
+                                name="description"
+                                type="text" 
+                                placeholder="Agregar descripcion."
+                                onBlur={handleBlur}
+                                isValid={touched.description && !errors.description}
+                                isInvalid={!!errors.description} 
+                            />
+                            <Form.Control.Feedback type="invalid">
+                                {errors.description}
+                            </Form.Control.Feedback>
+                        </Form.Group>
+                        <Form.Group controlId="formBasicImageUrl">
+                            <Form.Label>Url de Imagen</Form.Label>
+                            <Form.Control name="imageUrl"
+                                onChange={handleChange}
+                                value={values.imageUrl}
+                                type="text"
+                                placeholder="Agregar url de imagen del producto"
+                                onBlur={handleBlur}
+                                isValid={touched.imageUrl && !errors.imageUrl}
+                                isInvalid={!!errors.imageUrl}
+                            />
+                            <Form.Control.Feedback type="invalid">
+                                {errors.imageUrl}
+                            </Form.Control.Feedback>
+                        </Form.Group>
+                        <Modal.Footer>
+                            <Button variant="secondary" onClick={this.handleClose}>
+                                Close
+                            </Button>
+                            <Button variant="primary" type="submit">
+                                Agregar Producto
+                            </Button>
+                        </Modal.Footer>
                     </Form>
+                    )}
+                </Formik>
                 </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={this.handleClose}>
-                    Close
-                    </Button>
-                    <Button variant="primary" onClick={this.onSubmit}>
-                    Agregar Producto
-                    </Button>
-                </Modal.Footer>
             </Modal>
         </React.Fragment>
     );
